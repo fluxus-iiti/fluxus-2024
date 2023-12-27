f84992a426993f3b8c03edfaa582ad728aec529d
Ghost4681 <64606908+Ghost4681@users.noreply.github.com>
2023-12-27 22:44:43 +0530

    footer done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Technicals from '@/components/Technicals'
+import Footer from '@/components/Footer'
 export default function Home() {
   return (
     <div>
       
       <Technicals/>
+      <Footer/>
     </div>
   )
 }
