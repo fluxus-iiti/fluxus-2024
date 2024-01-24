ca260582c8a027091bd3eb883f4e4d0b55a7f651
HARRY <siddhji40@gmail.com>
2024-01-24 23:51:19 +0530

    ADDED TEAMS PAGE

--- src/components/Box.js
+++ src/components/Box.js
@@ -10,8 +10,8 @@ const BoxDay3 = () => {
   return (
     <div className="flex-1 flex flex-wrap justify-center w-1/2">
       <div className=" h-fit w-2/3 shadow-md border border-white relative text-center">
-        <div className="bg-black h-10 w-10 border rounded-full border-t-black border-l-black border-r-white absolute -top-[6px] -left-[6px]"></div>
-        <div className="bg-black h-10 w-10 border rounded-full border-t-black border-l-white border-b-white border-r-black  absolute -top-[6px] -right-[6px]"></div>
+        <div className="bg-black h-6 w-6 border rounded-full border-t-black border-l-black border-r-white absolute -top-[5px] -left-[5px]"></div>
+        <div className="bg-black h-6 w-6 border rounded-full border-t-black border-l-white border-b-white border-r-black  absolute -top-[5px] -right-[5px]"></div>
         {/* <img
               src="https://media.istockphoto.com/id/525430193/vector/sagittarius-constellation.jpg?s=612x612&w=0&k=20&c=xiPp51Hka5Vhzyt0yftgstOy1fu_b6ZWNC8MKRvoo_c="
               alt="Image 1"
@@ -26,8 +26,8 @@ const BoxDay3 = () => {
         <h2 className="text-base md:text-xl mb-20 text-white">
           LIFETIME EXPERIENCE
         </h2>
-        <div className="bg-black h-10 w-10 border rounded-full border-t-white border-l-white border-b-black border-r-black absolute -bottom-[6px] -right-[6px]"></div>
-        <div className="bg-black h-10 w-10 border rounded-full border-t-white border-l-black border-b-black border-r-white absolute -bottom-[6px] -left-[6px]"></div>
+        <div className="bg-black h-6 w-6 border rounded-full border-t-white border-l-white border-b-black border-r-black absolute -bottom-[5px] -right-[5px]"></div>
+        <div className="bg-black h-6 w-6 border rounded-full border-t-white border-l-black border-b-black border-r-white absolute -bottom-[5px] -left-[5px]"></div>
       </div>
     </div>
   );
