b29c2e4917c98f721d9b333b4a87f457e0784fab
Harsh <98403303+SoniHarsh1@users.noreply.github.com>
2024-01-24 14:28:48 +0530

    css fixez

--- src/components/main/About/TextSection.jsx
+++ src/components/main/About/TextSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const TextSection = () => {
   return (
-    <div className='basis-1/2 lg:basis-2/5 font-mate text-6xl md:text-7xl lg:text-[76px] underline decoration-4 leading-10 flex h-fit'>
+    <div className='basis-1/2 lg:basis-2/5 font-mate text-6xl md:text-7xl lg:text-[76px] underline decoration-4 leading-10 flex h-fit md:pr-10 my-16 md:my-0'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor  
     </div>
   )
