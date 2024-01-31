99fa90cfd22baa1edc095f4ff120c5ef0764f647
Krish Agrawal <diwankrish.17@gmail.com>
2024-01-31 20:34:10 +0530

    modifies about animations

--- src/components/main/About/About.jsx
+++ src/components/main/About/About.jsx
@@ -2,78 +2,27 @@
 import React from "react";
 import VideoSection from "./VideoSection";
 import TextSection from "./TextSection";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  useVelocity,
-  useAnimation,
-  useInView,
-} from "framer-motion";
-import { useEffect } from "react";
+import { motion, useScroll, useSpring } from "framer-motion";
+
 
 const About = () => {
-  const controls = useAnimation();
-  const [ref, inView] = useInView();
-  useEffect(() => {
-    if (inView) {
-      controls.start("visible");
+  const { scrollYProgress } = useScroll(
+    {
+      target: ref
     }
-    if (!inView) {
-      controls.start("hidden");
-    }
-  }, [controls, inView]);
-  const { scrollYProgress } = useScroll({
-    offset: ["end end", "start start"],
-  });
-  const divX = useTransform(
-    scrollYProgress,
-    [0, 0.3, 0.9],
-    ["-40vw", "0vw", "-20vw"]
   );
-  const divY = useTransform(
-    scrollYProgress,
-    [0, 0.3, 0.9],
-    ["0vh", "0vh", "20vh"]
-  );
-  const div2X = useTransform(
-    scrollYProgress,
-    [0, 0.3, 0.9],
-    ["20vw", "0vw", "-40vw"]
-  );
-  const div2Y = useTransform(scrollYProgress, [0, 0.3], ["0vh", "20vh"]);
-  const div2Opacity = useTransform(scrollYProgress, [0, 0.3], [0, 1]);
-  const div2Scale = useTransform(scrollYProgress, [0, 0.3], [2, 1]);
+  // const spring = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
+  const textX = useSpring(scrollYProgress, [0, 1], ["-100vw", "0vw"]);
 
   return (
-    <motion.div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center">
-      <motion.div
-        style={{ x: divX, y: divY }}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        transition={{
-          duration: 2.5,
-          type: "spring",
-          damping: 60,
-          mass: 0.5,
-          stiffness: 50,
-        }}
+    <div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center">
+      <motion.div 
+        style={{ x: textX }}
       >
         <TextSection />
       </motion.div>
-      <motion.div
-        style={{
-          x: div2X,
-          opacity: div2Opacity,
-          scale: div2Scale,
-          y: div2Y,
-        }}
-      >
-        <VideoSection />
-      </motion.div>
-    </motion.div>
+      <VideoSection />
+    </div>
   );
 };
 
