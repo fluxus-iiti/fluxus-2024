8c1110bbe237ba7839b10e28e7c3ea140d5c0a0f
Rupal17shah <shah17rupal@gmail.com>
2024-01-31 17:38:25 +0530

    added the intro and other sections

--- src/components/main/About/About.jsx
+++ src/components/main/About/About.jsx
@@ -1,15 +1,80 @@
-'use client'
-import React from 'react'
-import VideoSection from './VideoSection'
-import TextSection from './TextSection'
+"use client";
+import React from "react";
+import VideoSection from "./VideoSection";
+import TextSection from "./TextSection";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+  useAnimation,
+  useInView,
+} from "framer-motion";
+import { useEffect } from "react";
 
 const About = () => {
-  return (
-    <div className='flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center'>
-      <TextSection />
-      <VideoSection />
-    </div>
-  )
-}
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    }
+    if (!inView) {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+  const { scrollYProgress } = useScroll({
+    offset: ["end end", "start start"],
+  });
+  const divX = useTransform(
+    scrollYProgress,
+    [0, 0.3, 0.9],
+    ["-40vw", "0vw", "-20vw"]
+  );
+  const divY = useTransform(
+    scrollYProgress,
+    [0, 0.3, 0.9],
+    ["0vh", "0vh", "20vh"]
+  );
+  const div2X = useTransform(
+    scrollYProgress,
+    [0, 0.3, 0.9],
+    ["20vw", "0vw", "-40vw"]
+  );
+  const div2Y = useTransform(scrollYProgress, [0, 0.3], ["0vh", "20vh"]);
+  const div2Opacity = useTransform(scrollYProgress, [0, 0.3], [0, 1]);
+  const div2Scale = useTransform(scrollYProgress, [0, 0.3], [2, 1]);
 
-export default About
+  return (
+    <motion.div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center">
+      <motion.div
+        style={{ x: divX, y: divY }}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{
+          duration: 2.5,
+          type: "spring",
+          damping: 60,
+          mass: 0.5,
+          stiffness: 50,
+        }}
+      >
+        <TextSection />
+      </motion.div>
+      <motion.div
+        style={{
+          x: div2X,
+          opacity: div2Opacity,
+          scale: div2Scale,
+          y: div2Y,
+        }}
+      >
+        <VideoSection />
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default About;
--- src/components/main/IntroSection/IntroSection.jsx
+++ src/components/main/IntroSection/IntroSection.jsx
@@ -1,15 +1,37 @@
-'use client'
-import React from 'react'
-import IntroImage from './IntroImage'
-import IntroText from './IntroText'
+"use client";
+import React, { useRef } from "react";
+import IntroImage from "./IntroImage";
+import IntroText from "./IntroText";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useVelocity,
+} from "framer-motion";
 
 const IntroSection = () => {
-  return (
-    <div className='flex md:space-x-10 flex-col md:flex-row items-center'>
-        <IntroImage />
-        <IntroText />  
-    </div>
-  )
-}
+  // for intro section 1
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+  const divX = useTransform(
+    scrollYProgress,
+    [0, 0.3, 0.9],
+    ["-40vw", "0vw", "-20vw"]
+  );
+  const divY = useTransform(scrollYProgress, [0.3, 0.9], ["0vh", "-20vh"]);
 
-export default IntroSection
+  return (
+    <div className="flex md:space-x-10 flex-col md:flex-row items-center">
+      <motion.div style={{ y: divY, x: divX }}>
+        <IntroImage />
+      </motion.div>
+      <IntroText />
+    </div>
+  );
+};
+
+export default IntroSection;
