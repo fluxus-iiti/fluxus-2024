bc3c02bff60028ab8806a1860ef8a96d5cd0ea71
Krish Agrawal <diwankrish.17@gmail.com>
2024-01-31 23:27:40 +0530

    adds about text animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Hero from "@/webpages/Hero";
 import Navbar from "@/components/main/hero/Navbar";
 import { url } from "inspector";
 import Header from "@/webpages/Header";
-// import Events from "@/components/events/Events";
+import Events from "@/components/events/Events";
 
 import {
   motion,
@@ -23,116 +23,116 @@ import {
 import { ReactNode, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
-const sectionVariants: Variants = {
-  inView: {
-    transition: {
-      staggerChildren: 0.1,
-    },
-  },
-  outView: {
-    transition: {
-      staggerChildren: 0.1,
-      staggerDirection: -1,
-    },
-  },
-};
+// const sectionVariants: Variants = {
+//   inView: {
+//     transition: {
+//       staggerChildren: 0.1,
+//     },
+//   },
+//   outView: {
+//     transition: {
+//       staggerChildren: 0.1,
+//       staggerDirection: -1,
+//     },
+//   },
+// };
 
-const divsVariants: Variants = {
-  inView: {
-    opacity: 1,
-    scale: [null, 1.1, 1],
-    transition: {
-      duration: 0.5,
-    },
-  },
-  outView: {
-    scale: 1,
-    opacity: 0,
-    transition: {
-      duration: 0.5,
-    },
-  },
-};
+// const divsVariants: Variants = {
+//   inView: {
+//     opacity: 1,
+//     scale: [null, 1.1, 1],
+//     transition: {
+//       duration: 0.5,
+//     },
+//   },
+//   outView: {
+//     scale: 1,
+//     opacity: 0,
+//     transition: {
+//       duration: 0.5,
+//     },
+//   },
+// };
 
-function AnimationInView({
-  children
-}: {
-  children?: ReactNode;
-}) {
-  const { ref, inView } = useInView({
-    root: null,
-    threshold: 0.2,
-    rootMargin: "250px 0px -250px 0px",
-    delay: 100
-  });
+// function AnimationInView({
+//   children
+// }: {
+//   children?: ReactNode;
+// }) {
+//   const { ref, inView } = useInView({
+//     root: null,
+//     threshold: 0.2,
+//     rootMargin: "250px 0px -250px 0px",
+//     delay: 100
+//   });
 
-  return (
-    <motion.section
-      variants={sectionVariants}
-      animate={inView ? "inView" : "outView"}
-      initial="outView"
-      style={{ "--grad": 20 } as any}
-      ref={ref}
-    >
-      {Array.from({ length: 20 }, (_, k) => (
-        <motion.div
-          style={{ "--l": `${50 + k}%` } as any}
-          variants={divsVariants}
-          key={k}
-        >
-          {children}
-        </motion.div>
-      ))}
-    </motion.section>
-  );
-}
+//   return (
+//     <motion.section
+//       variants={sectionVariants}
+//       animate={inView ? "inView" : "outView"}
+//       initial="outView"
+//       style={{ "--grad": 20 } as any}
+//       ref={ref}
+//     >
+//       {Array.from({ length: 20 }, (_, k) => (
+//         <motion.div
+//           style={{ "--l": `${50 + k}%` } as any}
+//           variants={divsVariants}
+//           key={k}
+//         >
+//           {children}
+//         </motion.div>
+//       ))}
+//     </motion.section>
+//   );
+// }
 
-import AnimatedTest from '@/webpages/AnimatedTest'
+// import AnimatedTest from '@/webpages/AnimatedTest'
 export default function Home() {
-  const [containerSize, setContainerSize] = useState(0);
-  const containerRef = useRef<HTMLDivElement>(null!);
-  const { scrollY } = useScroll();
-  const transform = useTransform(
-    scrollY,
-    [0, containerSize],
-    [0, -containerSize]
-  );
+  // const [containerSize, setContainerSize] = useState(0);
+  // const containerRef = useRef<HTMLDivElement>(null!);
+  // const { scrollY } = useScroll();
+  // const transform = useTransform(
+  //   scrollY,
+  //   [0, containerSize],
+  //   [0, -containerSize]
+  // );
 
-  // add spring animation
-  const spring = useSpring(transform, { stiffness: 10, mass: 1 });
+  // // add spring animation
+  // const spring = useSpring(transform, { stiffness: 10, mass: 1 });
 
-  useEffect(() => {
-    const container = containerRef.current;
-    const body = document.body;
-    const size = container.getBoundingClientRect().height;
+  // useEffect(() => {
+  //   const container = containerRef.current;
+  //   const body = document.body;
+  //   const size = container.getBoundingClientRect().height;
 
-    // set the size of the body to be the sized of the container
-    // its needed to keep a scrollbar since the container has a fixed position
-    // see CSS
-    body.style.height = `${size}px`;
-    setContainerSize(size);
-  }, []);
+  //   // set the size of the body to be the sized of the container
+  //   // its needed to keep a scrollbar since the container has a fixed position
+  //   // see CSS
+  //   body.style.height = `${size}px`;
+  //   setContainerSize(size);
+  // }, []);
 
-  useLayoutEffect(() => {
-    const container = containerRef.current;
-    const observer = new ResizeObserver((entries) => {
-      for (const entry of entries) {
-        if (Array.isArray(entry.contentBoxSize)) {
-          setContainerSize(entry.contentRect.height);
-        } else {
-          setContainerSize(entry.contentRect.height);
-        }
-      }
-    });
+  // useLayoutEffect(() => {
+  //   const container = containerRef.current;
+  //   const observer = new ResizeObserver((entries) => {
+  //     for (const entry of entries) {
+  //       if (Array.isArray(entry.contentBoxSize)) {
+  //         setContainerSize(entry.contentRect.height);
+  //       } else {
+  //         setContainerSize(entry.contentRect.height);
+  //       }
+  //     }
+  //   });
 
-    observer.observe(container);
+  //   observer.observe(container);
 
-    return () => observer.disconnect();
-  }, []);
+  //   return () => observer.disconnect();
+  // }, []);
 
   return (
     <>
-      {/* <Events/> */}
+      <Events/>
       {/* <Hero/> */}
       {/* <About/> */}
       {/* <Technicals/> */}
@@ -143,14 +143,14 @@ export default function Home() {
       {/* <Navbar/>
       <Hero/> */}
 
-      <motion.div>
+      {/* <motion.div>
         <motion.div ref={containerRef} style={{ y: spring }} className="App">
           <Header />
           <About />
           <IntroSection />
           <AnimatedTest/>
         </motion.div>
-      </motion.div>
+      </motion.div> */}
     </>
   );
 }
--- src/components/main/About/About.jsx
+++ src/components/main/About/About.jsx
@@ -2,18 +2,23 @@
 import React from "react";
 import VideoSection from "./VideoSection";
 import TextSection from "./TextSection";
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const About = () => {
-  const ref = React.useRef(null);
+  const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
+    offset: ["0 1.33", "1 0"],
   });
-  // const spring = useSpring(scrollYProgress, { stiffness: 400, damping: 90 });
-  const textX = useSpring(scrollYProgress, [0, 1], ["-100vw", "0vw"]);
+  const springScroll = useSpring(scrollYProgress, {
+    stiffness: 50,
+    damping: 40,
+  });
+  const textX = useTransform(scrollYProgress, [0, 0.2], ["-100vw", "0vw"]);
 
   return (
-    <div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center">
+    <div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center" ref={ref}>
       <motion.div style={{ x: textX }}>
         <TextSection />
       </motion.div>
