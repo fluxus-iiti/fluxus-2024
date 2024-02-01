9f635c1783508063b5242fb84f5d79adf0f09d0c
Krish Agrawal <diwankrish.17@gmail.com>
2024-02-02 00:22:09 +0530

    fixes animations

--- src/components/main/hero/MainText.jsx
+++ src/components/main/hero/MainText.jsx
@@ -1,15 +1,20 @@
 "use client";
 import react, { useRef } from "react";
-import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 
 const AnimatedLetter = ({ letter, index, springScroll }) => {
   const delay = useTransform(
     springScroll,
-    [0, 0.25 - 0.001 * (11 - index), 0.4, 1],
-    [`${20 + 10 * index}vh`, "0vh", "0vh", `${200 - 10 * index}vh`]
+    [0, 0.3 - 0.001 * (11 - index), 0.6 + 0.001 * (11 - index), 1],
+    [`${20 + 10 * index}vh`, "0vh", "0vh", `${100 - 5 * index}vh`]
   );
 
-  if (letter === ' ') {
+  if (letter === " ") {
     return (
       <motion.div key={index} style={{ y: delay, display: "inline-block" }}>
         &nbsp;
@@ -28,7 +33,7 @@ const MainText = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["start end", "start start"],
+    offset: ["start end", "end start"],
   });
   const springScroll = useSpring(scrollYProgress, {
     stiffness: 300,
@@ -36,22 +41,30 @@ const MainText = () => {
   });
 
   const renderAnimatedLetters = (text) => {
-    return text.split("").map((letter, index) => (
-      <AnimatedLetter
-        key={index}
-        letter={letter}
-        index={index}
-        springScroll={springScroll}
-      />
-    ));
+    return text
+      .split("")
+      .map((letter, index) => (
+        <AnimatedLetter
+          key={index}
+          letter={letter}
+          index={index}
+          springScroll={springScroll}
+        />
+      ));
   };
 
   return (
-    <motion.div className="w-full pt-48 intro overflow-hidden" ref={ref}>
-      <motion.div className="text-white text-center lg:text-[20rem] border-0 w-full lg:!my-[-140px] !py-0 text-[35px] sm:text-[70px] sm:my-[-0px] font-serif">
+    <motion.div className="w-full pt-48 intro overflow-hidden">
+      <motion.div
+        className="text-white text-center lg:text-[20rem] border-0 w-full lg:!my-[-140px] !py-0 text-[35px] sm:text-[70px] sm:my-[-0px] font-serif"
+        ref={ref}
+      >
         {renderAnimatedLetters("A CELESTIAL")}
       </motion.div>
-      <div className="text-white text-center lg:text-[24rem] border-0 w-full lg:!my-[-100px] my-[-20px] text-[50px] !py-0 sm:text-[100px] sm:my-[-40px] overflow-hidden">
+      <div
+        className="text-white text-center lg:text-[24rem] border-0 w-full lg:!my-[-100px] my-[-20px] text-[50px] !py-0 sm:text-[100px] sm:my-[-40px] overflow-hidden"
+        ref={ref}
+      >
         <div>{renderAnimatedLetters("EUPHORIA")}</div>
       </div>
     </motion.div>
--- src/webpages/mainsite/About.jsx
+++ src/webpages/mainsite/About.jsx
@@ -1,32 +1,35 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useRef } from "react";
 import VideoSection from "../../components/main/About/VideoSection";
 import TextSection from "../../components/main/About/TextSection";
-import { motion, spring, useScroll, useSpring, useTransform } from "framer-motion";
-import { useRef } from "react";
+import {
+  motion,
+  useScroll,
+  useSpring,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
 
 const About = () => {
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["0 1", "1.33 1"],
+    offset: ["end end", "start start"],
   });
-  const springScroll = useSpring(scrollYProgress, {
-    stiffness: 50,
-    damping: 40,
-  });
-  const textX = useTransform(springScroll, [0, 1], ["-100vw", "0vw"]);
-
-  useEffect(() => {
-    console.log(scrollYProgress);
-  }, [scrollYProgress]);
+  const textX = useTransform(scrollYProgress, [0, 0.7], ["-100vw", "0vw"]);
+  const imgX = useTransform(scrollYProgress, [0, 0.7], ["-40vw", "0vw"]);
+  const imgScale = useTransform(scrollYProgress, [0, 0.7], [4, 1]);
+  const imgOpacity = useTransform(scrollYProgress, [0, 0.7], [0.5, 1]);
+  
 
   return (
-    <div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center">
+    <div className="flex md:space-x-10 flex-col md:flex-row md:ml-40 items-center overflow-hidden">
       <motion.div style={{ x: textX }} ref={ref}>
         <TextSection />
       </motion.div>
-      <VideoSection />
+      <motion.div style={{ x: imgX, scale: imgScale, opacity: imgOpacity }} ref={ref}>
+        <VideoSection />
+      </motion.div>
     </div>
   );
 };
