68040f162f4b52ee0666f7c16c5ba5aacb7ba861
Krish Agrawal <diwankrish.17@gmail.com>
2024-02-03 18:24:14 +0530

    navbar fixes

--- src/components/main/hero/Navbar.jsx
+++ src/components/main/hero/Navbar.jsx
@@ -1,38 +1,24 @@
 "use client";
 import Image from "next/image";
-import BurgerToCross from './BurgerToCross'
+import BurgerToCross from "./BurgerToCross";
 import logo from "../../../../public/images/Navbar/image1.png";
 import { motion } from "framer-motion";
-import OffCanvasNavbar from './OffCanvasNavbar'
+import OffCanvasNavbar from "./OffCanvasNavbar";
 export default function Navbar() {
   return (
     <>
-      <div id="burger_to_cross" className="top-5 !z-[100000000] absolute  left-5 w-full h-full my-auto flex font-weight-700 text-white text-2xl transition-all fade-in-out">
-          <BurgerToCross />
-        </div>
-    
-    <div className=" py-2 bg-black  overflow-hidden z-[-1]">
-      <div>
-        <OffCanvasNavbar />
+      <div
+        id="burger_to_cross"
+        className="top-5 !z-[100000000] absolute  left-5 w-full h-full my-auto flex font-weight-700 text-white text-2xl transition-all fade-in-out"
+      >
+        <BurgerToCross />
       </div>
 
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        transition={{
-          duration: 2.5,
-          type: "spring",
-          delay: 1,
-        }}
-        variants={{
-          visible: { y: "1vh", opacity: 1 },
-          hidden: { y: "0vh", opacity: 0 },
-        }}
-      >
-        
-      </motion.div>
-      <div className="w-full flex justify-center items-center">
+      <div className=" py-2 bg-[#050917] overflow-hidden z-[-1]">
+        <div>
+          <OffCanvasNavbar />
+        </div>
+
         <motion.div
           initial="hidden"
           whileInView="visible"
@@ -40,37 +26,51 @@ export default function Navbar() {
           transition={{
             duration: 2.5,
             type: "spring",
+            delay: 1,
           }}
           variants={{
-            visible: { y: "0vh", opacity: 1 },
-            hidden: { y: "-5vh", opacity: 0 },
+            visible: { y: "1vh", opacity: 1 },
+            hidden: { y: "0vh", opacity: 0 },
           }}
-        >
-          <Image
-            src={logo}
-            alt=""
-            placeholder="blur"
-            className="h-[70px] w-[35px] "
-          />
-        </motion.div>
+        ></motion.div>
+        <div className="w-full flex justify-center items-center">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{
+              duration: 2.5,
+              type: "spring",
+            }}
+            variants={{
+              visible: { y: "0vh", opacity: 1 },
+              hidden: { y: "-5vh", opacity: 0 },
+            }}
+          >
+            <Image
+              src={logo}
+              alt=""
+              placeholder="blur"
+              className="w-[5rem]"
+            />
+          </motion.div>
+        </div>
+
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          transition={{
+            duration: 2.5,
+            type: "spring",
+            delay: 1,
+          }}
+          variants={{
+            visible: { y: "1vh", opacity: 1 },
+            hidden: { y: "0vh", opacity: 0 },
+          }}
+        ></motion.div>
       </div>
-      
-      <motion.div
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        transition={{
-          duration: 2.5,
-          type: "spring",
-          delay: 1,
-        }}
-        variants={{
-          visible: { y: "1vh", opacity: 1 },
-          hidden: { y: "0vh", opacity: 0 },
-        }}
-      >
-      </motion.div>
-    </div>
     </>
   );
 }
--- src/components/main/hero/OffCanvasNavbar.jsx
+++ src/components/main/hero/OffCanvasNavbar.jsx
@@ -1,48 +1,57 @@
-'use client'
+"use client";
 import Image from "next/image";
 import { RxCross2 } from "react-icons/rx";
-import logo from '../../../../public/images/Navbar/image1.png'
+import logo from "../../../../public/images/Navbar/image1.png";
 import Link from "next/link";
 export default function OffCanvasNavbar() {
-    return (
-        <div id="offcanvas" className="bg-black py-2 transition-all fade-in-out  absolute top-[-500px] w-full  overflow-hidden !z-[1000]">
-
-            <div className="w-full flex flex-wrap  justify-center items-center">
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <Image
-                        src={logo}
-                        alt=""
-                        placeholder="blur"
-                        className="h-[70px] w-[35px] my-10 md:my-auto"
-                    />
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
-                        <Link className="text-2xl" href="technicals">Technicals</Link>
-                    </div>
-               
-            </div>
-        
+  return (
+    <div
+      id="offcanvas"
+      className="bg-[#050917] py-2 transition-all fade-in-out  absolute top-[-500px] w-full  overflow-hidden !z-[1000]"
+    >
+      <div className="w-full flex flex-wrap justify-center items-center">
+        <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto text-[#050917]">
+          {/* <Link className="text-2xl text-[#050917]" href='/' disable> */}
+            Technicals
+          {/* </Link> */}
         </div>
-    )
+        {/* <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div>
+                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div>
+                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div>
+                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div> */}
+        <Image
+          src={logo}
+          alt=""
+          placeholder="blur"
+          className="w-[5rem] my-10 md:my-auto mx-auto"
+        />
+        {/* <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div>
+                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div>
+                    <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+                        <Link className="text-2xl" href="technicals">Technicals</Link>
+                    </div> */}
+        <div className="md:mx-5 my-5 font-bold mx-10  text-2xl md:my-auto">
+          <Link className="text-2xl" href="technicals">
+            Technicals
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-{/*  */ }
+{
+  /*  */
+}
