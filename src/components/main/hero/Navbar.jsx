1868390b31788b761c2c5ab073a113b4ba4cedd0
PradeepKumarRebbavarapu <wardengaming1807@gmail.com>
2024-02-18 17:26:01 +0530

    changed navbar style

--- src/components/main/hero/Navbar.jsx
+++ src/components/main/hero/Navbar.jsx
@@ -8,27 +8,24 @@ import { FaBars } from "react-icons/fa";
 import { usePathname } from 'next/navigation'
 import Link from "next/link";
 import { useLayoutEffect } from "react";
+import NeonButton from "./NeonButton";
 export default function Navbar() {
   const pathname = usePathname()
   console.log(pathname)
 
   return (
     <div>
-      
 
-      <div id="navbar" className={`bg-transparent ${pathname == "/technicals" ? "absolute" : ""} w-full backdrop-blur-md md:backdrop-blur-none `}>
-        <div
-          id="burger_to_cross"
-          className="top-14 !z-[100000000] absolute  left-5 w-fit  my-auto  font-weight-700 text-white text-2xl transition-all fade-in-out  hidden lg:flex h-fit "
-        >
-          <BurgerToCross />
-        </div>
-        <div className="top-5 !z-[1000] absolute  left-5 w-full h-fit m-5 font-weight-700 text-white text-2xl transition-all fade-in-out lg:hidden">
+
+      <div id="navbar" className={`bg-transparent ${pathname == "/technicals" ? "absolute" : ""} w-full backdrop-blur-md md:backdrop-blur-none !z-[10000000000000000]`}>
+
+        <div id="bars" className="top-5 !z-[1000] absolute  left-5 w-full h-fit m-5 font-weight-700 text-white text-2xl transition-all fade-in-out lg:hidden">
           <FaBars className="sm:w-16 sm:h-16 h-10 w-10" onClick={() => {
-            document.getElementById('container').classList.toggle('nav-open');
+
             document.getElementById('offcanvas').style.top = "0px"
-            document.getElementById('burger_to_cross').style.transform = "translateY(50px)"
-            document.getElementById('navbar').style.visibility= "hidden"
+
+            // document.getElementById('navbar').style.visibility= "hidden"
+            document.getElementById('bars').style.visibility = "hidden"
           }} />
         </div>
 
@@ -50,33 +47,66 @@ export default function Navbar() {
             }}
           ></motion.div>
           <div className="w-full flex justify-center items-center">
-            <motion.div
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true }}
-              transition={{
-                duration: 2.5,
-                type: "spring",
-              }}
-              variants={{
-                visible: { y: "0vh", opacity: 1 },
-                hidden: { y: "-5vh", opacity: 0 },
-              }}
-            >
 
-              <div className="flex justify-center items-center Z ">
-                <Link href="/" className="cursor-pointer">
-                  <Image
-                    src={logo}
-                    alt=""
-                    placeholder="blur"
-                    className="w-[5rem] my-0 md:my-auto mx-10 border-0 border-red-500"
-                  />
-                </Link>
+            <div className="w-full lg:flex justify-center border-0 border-red-500 !text-4xl ">
+              <div className="w-full hidden lg:flex justify-end">
+                <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
+                  <Link className="text-2xl" href="/">Home</Link>
+                </div>
+                <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
+                  <Link className="text-2xl" target={"_blank"}  href="https://www.townscript.com/e/fluxus">Workshop</Link>
+                </div>
+                <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
+                  <Link className="text-2xl" href="/accommodation">Accommodation</Link>
+                </div>
+                {/* <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
+						<Link className="text-2xl" href="/accomodation">Accomodation</Link>
+					</div> */}
+
               </div>
+              <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{
+                  duration: 2.5,
+                  type: "spring",
+                }}
+                variants={{
+                  visible: { y: "0vh", opacity: 1 },
+                  hidden: { y: "-5vh", opacity: 0 },
+                }}
+              >
+                <div className="flex justify-center items-center Z ">
+                  <Link href="/" className="cursor-pointer">
+                    <Image
+                      src={logo}
+                      alt=""
+                      placeholder="blur"
+                      className="w-[5rem] my-0 md:my-auto mx-10 border-0 border-red-500"
+                    />
+                  </Link>
+                </div>
+              </motion.div>
+              <div className="w-full border-0 lg:flex flex-col hidden items-center lg:flex-row justify-between ml-14">
+                <div className="flex w-full">
+                  <div className={`md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto transition-all fade-in-out hover:!text-gray-300 hover:border-b-2 backdrop-blur-md md:backdrop-blur-none`}>
+                    <Link className="text-2xl" target={"_blank"} href="https://ca.fluxus.co.in/">CA Fluxus</Link>
+                  </div>
+                  <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
+                    <Link className="text-2xl" href="technicals">Technicals</Link>
+                  </div>
+                </div>
+                <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto transition-all fade-in-out hover:!text-gray-300">
+                  <Link className="text-2xl" target={"_blank"} href="https://forms.gle/oETzssE9asLVme8Y8"><NeonButton/></Link>
+                </div>
 
-            </motion.div>
+
+              </div>
+            </div>
+
           </div>
+
 
           <motion.div
             initial="hidden"
--- src/components/main/hero/OffCanvasNavbar.jsx
+++ src/components/main/hero/OffCanvasNavbar.jsx
@@ -5,35 +5,26 @@ import logo from "../../../../public/images/Navbar/image1.png";
 import Link from "next/link";
 import { IoClose } from "react-icons/io5";
 import { usePathname } from "next/navigation";
+import NeonButton from './NeonButton'
 export default function OffCanvasNavbar() {
 	const pathname = usePathname()
 	return (
 		<div
 			id="offcanvas"
-			className={` py-4 transition-all fade-in-out  absolute top-[-5000px] w-full   overflow-hidden  bg-black lg:bg-transparent bg-opacity-80 ${pathname=="/technicals"?"!z-[1000000]":""}`}
+			className={` py-4 transition-all fade-in-out  absolute top-[-5000px] lg:hidden w-full   overflow-hidden  bg-black lg:bg-transparent bg-opacity-80 ${pathname=="/technicals"?"!z-[1000000]":""}`}
 		>
 
 			<div className="w-full lg:flex justify-center border-0 border-red-500 !text-4xl ">
 				{/* the below div is the left side navigation buttons in desktop view */}
-				<div className="w-full hidden lg:flex justify-end">
-					<div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
-						<Link className="text-2xl" href="/">Home</Link>
-					</div>
-					<div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
-						<Link className="text-2xl" href="/accommodation">Accommodation</Link>
-					</div>
-					{/* <div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
-						<Link className="text-2xl" href="/accomodation">Accomodation</Link>
-					</div> */}
-
-				</div>
+				
 				<div className="flex justify-center items-center relative ">
 					<div className="top-0  absolute  left-0 w-full h-full m-5 font-weight-700 text-white text-2xl transition-all fade-in-out lg:hidden" onClick={() => {
 
-						document.getElementById('container').classList.toggle('nav-open');
+						
 						document.getElementById('offcanvas').style.top = "-5000px"
-						document.getElementById('burger_to_cross').style.transform = "translateY(0px)"
-						document.getElementById('navbar').style.visibility= "visible"
+						
+						
+						document.getElementById('bars').style.visibility= "visible"
 
 
 
@@ -58,27 +49,25 @@ export default function OffCanvasNavbar() {
 						<Link className="text-2xl" href="/">Home</Link>
 					</div>
 					<div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
+						<Link target={"_blank"}  className="text-2xl" href="https://www.townscript.com/e/fluxus">Workshop</Link>
+					</div>
+					<div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
 						<Link className="text-2xl" href="/accommodation">Accommodation</Link>
 					</div>
 					<div className={`md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto transition-all fade-in-out hover:!text-gray-300 hover:border-b-2 backdrop-blur-md md:backdrop-blur-none`}>
-						<Link className="text-2xl" href="https://ca.fluxus.co.in/">CA Fluxus</Link>
+						<Link target={"_blank"}  className="text-2xl" href="https://ca.fluxus.co.in/">CA Fluxus</Link>
 					</div>
 					<div className={`md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto transition-all fade-in-out hover:!text-gray-300 hover:border-b-2`}>
 						<Link className="text-2xl" href="/technicals">Technicals</Link>
+					</div>
+					<div className={`md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto transition-all fade-in-out hover:!text-gray-300 hover:border-b-2`}>
+					<Link className="text-2xl" target={"_blank"} href="https://forms.gle/oETzssE9asLVme8Y8"><NeonButton/></Link>
 					</div>
 
 				</div>
 
 				{/* below div is for desktop view right side navigation buttons , right side of the logo */}
-				<div className="w-full lg:flex flex-col hidden items-center lg:flex-row justify-start">
-					<div className={`md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto transition-all fade-in-out hover:!text-gray-300 hover:border-b-2 backdrop-blur-md md:backdrop-blur-none`}>
-						<Link className="text-2xl" href="https://ca.fluxus.co.in/">CA Fluxus</Link>
-					</div>
-					<div className="md:mx-5 my-5 font-bold mx-10  !text-4xl md:my-auto hover:border-b-2 transition-all fade-in-out hover:!text-gray-300">
-						<Link className="text-2xl" href="technicals">Technicals</Link>
-					</div>
-
-				</div>
+				
 
 
 			</div>
