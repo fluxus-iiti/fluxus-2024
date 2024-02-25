d1297725700c11d9ff29ae7c28f4bc437699425e
Krish Agrawal <diwankrish.17@gmail.com>
2024-02-25 15:36:36 +0530

    fixes team images error

--- src/components/team/MemberCard.jsx
+++ src/components/team/MemberCard.jsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import React from "react";
 import { SocialIcon } from "react-social-icons/component";
 import "react-social-icons/linkedin";
 import "react-social-icons/email";
@@ -23,7 +23,7 @@ const MemberCard = ({ member, isHead }) => {
       }}
     >
       <div className="relative">
-        <Image
+        <img
           src={member.imagePath}
           alt={member.name}
           className="w-full h-auto rounded-t-xl"
